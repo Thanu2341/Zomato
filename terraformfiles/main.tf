d74e513c9deb86e4585aa9756832bfd29cf24ae7
Thanu2341 <thanushree5667@gmail.com>
2025-11-23 12:19:06 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,19 +1,19 @@
 resource "aws_instance" "test-server" {
-  ami                    = "ami-0fa3fe0fa7920f68e"
+  ami                    = "ami-0fa3fe0fa7920f68e_us-east-1"
   instance_type          = "t3.small"
   key_name               = "thetfkey"
   vpc_security_group_ids = ["sg-0b4273bc0b0a56bdf"]
+
+  subnet_id              = "subnet-06434fdca0fcadee0"   # ← ID of an existing subnet in that VPC
 
   tags = {
     Name = "test-server"
   }
 
-  # Write instance IP to an inventory file
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > inventory"
   }
 
-  # Run Ansible playbook using that inventory
   provisioner "local-exec" {
     command = "ansible-playbook -i inventory /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
   }
