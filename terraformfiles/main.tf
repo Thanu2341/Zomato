2d0c518d6911a03ab5a0658b3f8d9bf17494a00e
Thanu2341 <thanushree5667@gmail.com>
2025-11-23 12:02:29 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,24 +1,20 @@
 resource "aws_instance" "test-server" {
-ami = "ami-0fa3fe0fa7920f68e"
-instance_type = "t3.small"
-key_name = "thetfkey"
-vpc_security_group_ids = ["sg-0b4273bc0b0a56bdf"]
-connection {
-  type = "ssh"
-  user = "ec2-user"
-  private_key = file("./thetfkey.com")
-  host = self.public_ip
+  ami                    = "ami-0f3aef0a7902f68e"
+  instance_type          = "t3.small"
+  key_name               = "thetfkey"
+  vpc_security_group_ids = ["sg-0b4273bc0b0a56bdf"]
+
+  tags = {
+    Name = "test-server"
   }
-provisioner "remote-exec" {
-  inline = ["echo 'wait to start the instance' "]
-}
-tags = {
-  name = "test-server"
+
+  # Write instance IP to an inventory file
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > inventory"
   }
-provisioner "local-exec" {
-  command = "echo ${aws_instance.test-server.public_ip} > inventory"
-  }
-provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
+
+  # Run Ansible playbook using that inventory
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
   }
 }
