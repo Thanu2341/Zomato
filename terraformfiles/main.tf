db8ac0bef733c8d25c95af159491888b272458f8
Thanu2341 <thanushree5667@gmail.com>
2025-11-23 12:10:11 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test-server" {
-  ami                    = "ami-0f3aef0a7902f68e"
+  ami                    = "ami-0fa3fe0fa7920f68e"
   instance_type          = "t3.small"
   key_name               = "thetfkey"
   vpc_security_group_ids = ["sg-0b4273bc0b0a56bdf"]
